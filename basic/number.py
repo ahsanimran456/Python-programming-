347905a2e582f952c4e4aef9f161357553ffef47
AhXan <ahxanimran456@gmail.com>
2025-05-10 16:30:44 +0500

    day2

---
+++ basic/number.py
@@ -0,0 +1,34 @@
+
+import math
+import  random
+
+a = math.floor(3.5)
+# print (a)
+
+b= math.ceil(3.2)
+# print (b)
+
+c = math.trunc(3.9)
+# print (c)
+
+d = math.factorial(5)
+# print (d)
+
+# random = math.random()
+# print (random)
+
+
+# random_number = random.randint(1, 10)
+# # print(random_number)
+
+# choice = random.choice([1, 2, 3, 4, 5])
+# # print(choice)
+
+# random_suffle = random.sample([1, 2, 3, 4, 5], 3)
+# print(random_suffle)
+
+
+
+
+strip = "   Hello World   "
+# print(strip.strip())
